98b67d6f5280670d81f8dedbd218525ece64f186
andificus <255278335+andificus@users.noreply.github.com>
2026-01-16 20:12:55 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from './components/NavBar'
+import ThemeProvider from './ThemeProvider'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider />
         <NavBar />
         {children}
       </body>
